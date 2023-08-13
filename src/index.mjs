cc85a7abefa1140f6abea3a9870a20641f62a0dd
p09057 <55584599+p09057@users.noreply.github.com>
2023-08-13 16:27:43 +0900

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,31 @@
+// var val1="var変数";
+// console.log(val1);
+
+// val1= " var変数を上書き";
+// console.log(val1);
+
+// var val1="val1を再宣言"
+// console.log(val1);
+
+let val2 = "let変数"
+console.log(val2);
+
+val2 = "let変数を上書き"
+console.log(val2);
+
+// let val2 = "let変数を再宣言"
+
+const val4 = {
+  name: "xxx",
+  age: 28,
+};
+console.log(val4);
+val4.name="yyy";
+console.log(val4);
+
+const val5 = ['dog','cat'];
+console.log(val5);
+val5[0]='bird';
+console.log(val5);
+val5.push('monkey');
+console.log(val5);
